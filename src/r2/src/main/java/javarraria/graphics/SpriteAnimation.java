5d3cd02fd2166ce97248277c9ea7a1b7d05cdc00
John Kayne <johnhoganuke2@gmail.com>
2017-06-30 18:12:36 -0400

    Create SpriteAnimation.java

---
+++ src/r2/src/main/java/javarraria/graphics/SpriteAnimation.java
@@ -0,0 +1,24 @@
+package javarraria.graphics;
+
+public class SpriteAnimation extends ArrayList<BufferedImage> {
+  public enum RepeatType {
+    STOP,LOOP,REVERSE;
+  
+    private RepeatType() {
+      
+    }
+  }
+
+  private RepeatType repeatType;
+  private int index = 0;
+  private int incr = 1;
+
+  public void tick() {
+    index+=incr;
+    if (index >= size()) {
+      if (repeatType == RepeatType.STOP) index = size()-1;
+      else if (repeatType == RepeatType.LOOP) index -= size();
+      else if (repeatType == RepeatType.REVERSE) {index = size()-2;incr=-incr;}
+    }
+  }
+}
